33f16e6cccd1695f28d131f7d09729229b51c2df
Sean Monahan <1907654+seancmonahan@users.noreply.github.com>
2021-04-07 19:41:47 -0400

    normalize output so adding harmonics doesn't affect output volume, especially causing clipping

--- src/app/curves.ts
+++ src/app/curves.ts
@@ -25,7 +25,7 @@ function calculateSample(
   const angularFrequency = frequency * 2 * Math.PI;
   // What's the "time" of this sample in our curves?
   const sampleTime = sampleNumber / sampleRate;
-  // What's the angle of the oscillator at this time? 
+  // What's the angle of the oscillator at this time?
   const sampleAngle = sampleTime * angularFrequency;
   // What's the value of the sinusoid for this angle?
   return amplitude * Math.sin(sampleAngle);
@@ -35,18 +35,26 @@ function calculateSample(
 // Combine a number of wave partials.
 export function combineCurves(partials: List<List<number>>, gain: number) {
   const sampleCount = partials.first().size;
-  return Range(0, sampleCount)
-    .map(sampleNumber => calculateCombinedSample(partials, sampleNumber, gain));
+  const combinedSamples = Range(0, sampleCount)
+    .map(sampleNumber => calculateCombinedSample(partials, sampleNumber)).toList();
+  return normalized(combinedSamples, gain);
 }
 
 // Calculate a single sample for the combined curve of a number of partials.
-function calculateCombinedSample(partials: List<List<number>>, sampleNumber: number, gain: number) {
+function calculateCombinedSample(partials: List<List<number>>, sampleNumber: number) {
   // Sum up the values of all the sine waves for this particular sample.
   const sum = partials
     .map(p => p.get(sampleNumber))
     .reduce((sum, s) => sum + s, 0);
-  // Apply master gain to the sum.
-  const sample = sum * gain;
-  // Clamp to [-1..1]
-  return Math.min(1, Math.max(-1, sample));
+  return sum;
+}
+
+export function normalized(combinedSamples: List<number>, gain: number) {
+  const sampleCount = combinedSamples.size;
+  const rootMeanSquare = Math.sqrt(
+    combinedSamples
+      .map(s => s * s)
+      .reduce((sum, s) => sum + s, 0) / sampleCount);
+  const k = gain / rootMeanSquare;
+  return combinedSamples.map(s => s * k);
 }
